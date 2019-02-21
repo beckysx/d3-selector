83f324f328efbbe342f930fed0699ec4f7eab2cf
beckysx <47224761+beckysx@users.noreply.github.com>
2019-02-21 16:20:21 -0500

    Update step2.js

--- step2.js
+++ step2.js
@@ -1,3 +1,15 @@
+/*  step1  */
+var s = document.createElement("script")
+s.src = "https://d3js.org/d3.v5.js"
+var body = document.getElementsByTagName("body")
+var body = document.getElementsByTagName("body")[0]
+body.after(s)
+d3.selectAll("div.fusion-text a").nodes().forEach(function(e){console.log(e.innerText)})
+
+
+
+
+/*  step2  */
 var s=document.createElement("script")
 s.src="https://d3js.org/d3.v5.js"
 var body=document.getElementsByTagName("body")
